80471cd382d5dd8d878cd3b9d76951a272f30b1f
AckonSamuel <ackons433@gmail.com>
2022-04-01 00:25:30 +0100

    Refactor TodoItem component to  use funtion component style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,6 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-    render() {
-        return (
-            <li>{this.props.todo.title}</li>
-        )
-    }
+function TodoItem(props) {
+    return <li>{props.todo.title}</li>
 }
 export default TodoItem;
