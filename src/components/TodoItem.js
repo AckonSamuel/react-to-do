b24a6f5e81e25dccccc79809ab6a1d12587f4ae0
AckonSamuel <ackons433@gmail.com>
2022-04-01 00:15:01 +0100

    Pass data as props

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <>
+            <h1>hola!</h1>
+            </>
+        )
+    }
+}
+export default TodoItem;
